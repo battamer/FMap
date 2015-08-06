f10a3ab288560b6d8e5311a2eac46b68109f6349
MAtt Baer <battamer@gmail.com>
2015-08-07 02:29:18 +0300

    improves parsing - includes seconds

--- Data_parsers/parse_state_boundries.py
+++ Data_parsers/parse_state_boundries.py
@@ -14,11 +14,12 @@ def dmDeg(degree, minute, direction):
     return sign*(degree+(minute/60.0))
 
 file_loc = 'state_boundries_raw.txt'
-search_string = u"(\d+)\u00b0( (\d+)')? ([WENS]) to "+\
-        u"(\d+)\u00b0( (\d+)')? ([WENS])" 
+search_string = u"(\d+)\u00b0( (\d+)')?( (\d+)\")? ([WENS]) to "+\
+        u"(\d+)\u00b0( (\d+)')?( (\d+)\")? ([WENS])" 
 f = codecs.open(file_loc, encoding='utf-8')
 
 print '#State,lllon,urlon,lllat,urlat'
+print "USA,-64,-119,22,49"
 output = ''
 line_count = 0
 
@@ -33,13 +34,18 @@ for line in f:
 
     else:
         re_res = re.search(search_string, line)
-        ll_deg, ll_min, ll_dir= \
-                int(re_res.group(1)), re_res.group(3), re_res.group(4)
+        ll_deg, ll_min, ll_sec, ll_dir= \
+                    int(re_res.group(1)), re_res.group(3), 
+                    re_res.group(5), re_res.group(6)
         ll_min = int(ll_min) if ll_min else 0
+        ll_sec = int(ll_sec) if ll_sec else 0
 
-        ur_deg, ur_min, ur_dir= \
-                int(re_res.group(5)), re_res.group(7), re_res.group(8)
+        ur_deg, ur_min, ur_sec, ur_dir= \
+                int(re_res.group(7)), re_res.group(9),
+                re_res.group(11), re_res.group(122)
         ur_min = int(ur_min) if ur_min else 0
+        ur_sec = int(ur_sec) if ur_sec else 0
+
         output += str(dmDeg(ll_deg, ll_min, ll_dir))+','
         output += str(dmDeg(ur_deg, ur_min, ur_dir))
 
